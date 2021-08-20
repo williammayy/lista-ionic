f7e041f9765d1a0ba4481133d7c8c5f645c29be6
William May <williammayy@gmail.com>
2021-08-20 15:39:27 -0300

    adicionando todos arquivos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  termoDeBusca: string;
+
+  public veiculos;
+  constructor() {
+    this.veiculos=[
+      {modelo:'GOL', marca:'Volkswagen', ano:2011},
+      {modelo:'Voyage', marca:'Volkswagen', ano:2010},
+      {modelo:'FOX', marca:'Volkswagen', ano:2008},
+      {modelo:'Onix', marca:'Chevrolet', ano:2017},
+      {modelo:'Palio', marca:'FIAT', ano:2011},
+      {modelo:'UNO', marca:'FIAT', ano:1994},
+      {modelo:'Siena', marca:'FIAT', ano:2005},
+      {modelo:'Meriva', marca:'Chevrolet', ano:2013},
+      {modelo:'Celta', marca:'Chevrolet', ano:2010},
+      {modelo:'Toro', marca:'FIAT', ano:2021},
+      {modelo:'HB20', marca:'Hyundai', ano:2019},
+      {modelo:'Corsa', marca:'Chevrolet', ano:2009},
+      {modelo:'Sandero', marca:'Renault', ano:2018},
+
+    ]
+
+  }
 
 }
